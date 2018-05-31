4ebbf8c295ee76731f219aef1f93c1655f5159b9
Dan Shreim <dan@snapjay.com>
2018-05-31 15:23:58 -0400

    Clean up css

--- src/components/grid-item/grid-item.vue
+++ src/components/grid-item/grid-item.vue
@@ -14,13 +14,13 @@
     div.mb-5.pd-grid-item
         .border.border-light.mb-3.position-relative
             .pd-grid-badge.bg-primary.text-white.text-truncate(v-text='badge', v-if="badge", :title="badge")
-            img(:style='`background-image:url(${image})`', src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==")
+            img.image(:style='`background-image:url(${image})`', src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==")
             slot(name="image")
         slot
 
 </template>
 <style scoped>
-    img {
+    img.image {
         width: 100%;
         background-size: cover;
         background-repeat: no-repeat;
