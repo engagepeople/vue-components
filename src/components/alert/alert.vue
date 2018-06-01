762e9ba37fd73c73809ee8f7823f6cd3a4678636
Dan Shreim <dan@snapjay.com>
2018-06-01 15:24:48 -0400

    Clean up alert SVG

--- src/components/alert/alert.vue
+++ src/components/alert/alert.vue
@@ -23,7 +23,7 @@
                         v-if="variant === 'success'",
                         xmlns='http://www.w3.org/2000/svg',
                         viewbox='0 0 41 41')
-                            title check
+                            title(v-text='title')
                             circle.success(cx='20.5', cy='20.5', r='20')
                             polyline.success(points='31 13.5 17 27.5 10 20.5')
 
@@ -31,11 +31,10 @@
                         v-if="variant === 'warning'",
                         xmlns='http://www.w3.org/2000/svg',
                         viewbox='0 0 41 41')
-                            title alert
+                            title(v-text='title')
                             circle.alert(cx='20.5', cy='20.5', r='20')
                             line.alert(x1='20.5', y1='28.5', x2='20.5', y2='30.5')
                             line.alert(x1='20.5', y1='10.5', x2='20.5', y2='25.5')
-
 
                     span.text-center(v-text='title')
             .link.text-center.px-4.py-3.bg-white.border-top-0(:class="`alert-${this.variant}`", v-if="$slots.default")
@@ -46,17 +45,17 @@
         border-width: 1px;
         border-style: solid;
     }
-    svg {
+    .alert-heading svg {
         width: 41px;
         height: 41px;
     }
-    svg .success {
+    .alert-heading svg .success {
         fill: none;
         stroke: #00b194;
         stroke-miterlimit: 10;
     }
 
-    svg .alert {
+    .alert-heading svg .alert {
         fill: none;
         stroke: #ffaf3e;
         stroke-miterlimit: 10;
