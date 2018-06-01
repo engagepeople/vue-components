5a36f139f0e3fc2d60634187eee9c34076b6a07a
Sergey Uss <suss@engagepeople.com>
2018-06-01 13:57:18 -0400

    alert icon margin fix

--- src/components/alert/alert.vue
+++ src/components/alert/alert.vue
@@ -18,22 +18,23 @@
         .mb-4
             .alert.alert-primary.mb-0(role="alert", :class="`alert-${this.variant}`")
                 h3.alert-heading.mb-2.d-flex.align-items-center.justify-content-center.flex-wrap
-                    svg.mr-3(
-                    v-if="variant === 'success'",
-                    xmlns='http://www.w3.org/2000/svg',
-                    viewbox='0 0 41 41')
-                        title check
-                        circle.success(cx='20.5', cy='20.5', r='20')
-                        polyline.success(points='31 13.5 17 27.5 10 20.5')
+                    .mx-3
+                        svg(
+                        v-if="variant === 'success'",
+                        xmlns='http://www.w3.org/2000/svg',
+                        viewbox='0 0 41 41')
+                            title check
+                            circle.success(cx='20.5', cy='20.5', r='20')
+                            polyline.success(points='31 13.5 17 27.5 10 20.5')
 
-                    svg.mr-3(
-                    v-if="variant === 'warning'",
-                    xmlns='http://www.w3.org/2000/svg',
-                    viewbox='0 0 41 41')
-                        title alert
-                        circle.alert(cx='20.5', cy='20.5', r='20')
-                        line.alert(x1='20.5', y1='28.5', x2='20.5', y2='30.5')
-                        line.alert(x1='20.5', y1='10.5', x2='20.5', y2='25.5')
+                        svg(
+                        v-if="variant === 'warning'",
+                        xmlns='http://www.w3.org/2000/svg',
+                        viewbox='0 0 41 41')
+                            title alert
+                            circle.alert(cx='20.5', cy='20.5', r='20')
+                            line.alert(x1='20.5', y1='28.5', x2='20.5', y2='30.5')
+                            line.alert(x1='20.5', y1='10.5', x2='20.5', y2='25.5')
 
 
                     span.text-center(v-text='title')
