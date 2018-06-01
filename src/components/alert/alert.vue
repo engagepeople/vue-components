0bf1293003814688473d6a3afb59046a96bb0bbd
Dan Shreim <dan@snapjay.com>
2018-06-01 09:34:30 -0400

    Remove Padding from alert

--- src/components/alert/alert.vue
+++ src/components/alert/alert.vue
@@ -16,7 +16,7 @@
 <template lang="pug">
     div.pd-alert
         .mb-4
-            div.alert.alert-primary.mt-5.mb-0(role="alert", :class="`alert-${this.variant}`")
+            div.alert.alert-primary.mb-0(role="alert", :class="`alert-${this.variant}`")
                 h3.alert-heading.mb-2.text-center
                     img.mr-3(src='/static/images/icons/alert.svg', v-if="variant === 'warning'")
                     img.mr-3(src='/static/images/icons/check.svg', v-if="variant === 'success'")
