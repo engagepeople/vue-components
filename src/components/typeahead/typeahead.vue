0f0d6c580015fb4b014aead166a777835ecc8095
Sergey Uss <suss@engagepeople.com>
2018-06-04 11:43:23 -0400

    Fixing typo in class name

--- src/components/typeahead/typeahead.vue
+++ src/components/typeahead/typeahead.vue
@@ -216,7 +216,7 @@
     }
 </script>
 <template lang="pug">
-    .pd-typeahed
+    .pd-typeahead
         .border.border-light.p-1.d-flex.flex-wrap
             .result-block.bg-primary.text-white.py-1.px-3.m-1.d-flex.align-items-center(v-for="(item, key) in results", :key="key")
                 .label(v-text="$scopedSlots['result-text'] ? $scopedSlots['result-text']({item})[0].text : item[internalKeys.resultLabel]")
