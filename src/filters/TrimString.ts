ecb3be0a40833ccb665d48d36453987b19eb8af6
s <agafonov.sergey@gmail.com>
2018-07-27 12:34:56 -0400

    trim string by number of words

--- src/filters/TrimString.ts
+++ src/filters/TrimString.ts
@@ -1,9 +1,14 @@
-export default function(str: string, length: number = 100, omission: string = '...'): string {
+export default function(str: string, wordsCount: number = 15, omission: string = '...'): string {
     if (typeof str !== 'string') {
         return str
     }
-    if (str.length <= length) {
+
+    const words = str.trim().split(/\s+/)
+    if (words.length <= wordsCount) {
         return str
     }
-    return str.substring(0, 100) + omission
+
+    const lastIncludedWord = words[wordsCount - 1]
+
+    return str.slice(0, str.indexOf(lastIncludedWord) + lastIncludedWord.length) + omission
 }
