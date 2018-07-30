84a1e2af355040a27cba4f8cf3ae366776f834fe
s <agafonov.sergey@gmail.com>
2018-07-30 16:16:26 -0400

    trim string: don't allow wordsCount < 1

--- src/filters/TrimString.ts
+++ src/filters/TrimString.ts
@@ -2,6 +2,9 @@ export default function(str: string, wordsCount: number = 15, omission: string =
     if (typeof str !== 'string') {
         return str
     }
+    if (wordsCount < 1) {
+        throw new Error('wordsCount can\'t be less than 1')
+    }
 
     const words = str.trim().split(/\s+/)
     if (words.length <= wordsCount) {
